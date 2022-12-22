2daf9f699c31a83b43d5c8218cb1ead2d6a07d1f
Frank Pham <fphamsd@Franks-MBP.lan>
2022-12-21 18:34:01 -0800

    Adding hero and headline components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import HeadlineCards from "./components/HeadlineCards";
+import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Hero />
+      <HeadlineCards />
     </div>
   );
 }
